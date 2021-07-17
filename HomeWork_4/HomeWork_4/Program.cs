1e558308ae7157a0af26569beb5a8f15698fb8f1
ChivononGeko <74726135+ChivononGeko@users.noreply.github.com>
2021-07-17 14:36:02 +0600

    Add files via upload

--- HomeWork_4/HomeWork_4/Program.cs
+++ HomeWork_4/HomeWork_4/Program.cs
@@ -12,16 +12,16 @@ namespace HomeWork_4
         {
             //Создаем экземпляр рандом
             Random random = new Random();
-            
+
             //Пременная для хранения положителььных значений
             int positiveCount = 0;
-            
+
             //Двумерный массив, где будут храниться финансовые данные
             int[,] finance = new int[12, 4];
-            
+
             //Для понимания таблицы
             Console.WriteLine($"Месяц  Доход(тыс.руб.) Расход(тыс.руб.)  Прибль(тыс.руб.)");
-            
+
             //Вложенный счетчик для заполнения массива
             for (int i = 0; i < finance.GetLength(0); i++)
             {
@@ -41,7 +41,7 @@ namespace HomeWork_4
                         // Подсчет разности доходов и расходов
                         finance[i, j] = finance[i, j - 2] - finance[i, j - 1];
                         Console.Write($"{finance[i, j],18}");
-                        
+
                         //Тут же поиск положительных значений
                         if (finance[i, j] > 0)
                         {
@@ -59,39 +59,59 @@ namespace HomeWork_4
             Console.WriteLine($"Количество месяцев с положительной прибылью: {positiveCount}");
 
             //Создаем отдельный массив, для данных прибыли
-            int[]profit = new int[12];
-            
+            int[] profit = new int[12];
+
             //Копируем из общего массива данных, в отдельный массив прибыли
             for (int i = 0; i < finance.GetLength(0); i++)
-            {                               
+            {
                 profit[i] = finance[i, 3];
             }
             //Сортируем массив прыбили
             Array.Sort(profit);
-          
+
             //Переменная, отвечающая за количесвто итераций
             int n = 3;
-            
+
             //Переменнная, в которой будет храниться количество месяцев с минимальной прибылью
             int count = 0;
-           
+
+            int[] profitMin = new int[12];
+            int k = 0;
+
             //Выводим первые три значения, так каак они булут минимальными. Но в случае равенства, пропускаем вывод и увеличиваем количество итераций
-            Console.WriteLine($"Три месяца с наименьшей прибылью:");
             for (int i = 0; i < n; i++)
             {
-                if (profit[i]!=profit[i+1])
+                if (n<12)
                 {
-                    Console.WriteLine(profit[i]);
-                    count++;
+                    if (profit[i] != profit[i + 1])
+                    {
+                        profitMin[k] = profit[i];
+                        count++;
+                        k++;
+                    }
+                    else
+                    {
+                        count++;
+                        n++;
+                    }
                 }
                 else
                 {
-                    n++;
-                    count++;
+                    count=12;
+                    Console.WriteLine($"Все месяцы являются минимальными и составялют: {profit[0]}");
+                    break;
                 }
-                
+
+            }
+            if (count != 12)
+            {
+                Console.WriteLine("Три месяца с наименьшей прибылью: ");
+                for (int i = 0; i < 3; i++)
+                {                   
+                        Console.WriteLine(profitMin[i]);                  
+                }
             }
             Console.WriteLine("Количество месяцев с минимальной прибылью:" + count);
-        }        
+        }
     }
 }
