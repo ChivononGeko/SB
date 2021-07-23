535d97af4efebabbae6cdaf974c2a725c23a32fd
ChivononGeko <74726135+ChivononGeko@users.noreply.github.com>
2021-07-23 15:02:35 +0600

    Add files via upload

---
+++ HomeWork_5.2/HomeWork_5.2/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork_5._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Для удобства я хотел разбирать одно и то же предложение
+            string a = Console.ReadLine();
+
+            Console.WriteLine("Слова с минимальным кол-ом букв");
+            Print(MinWord(a));
+
+            Console.WriteLine("Слова с максимальным кол-ом букв");
+            Print(MaxWord(a));
+        }
+        /// <summary>
+        /// Метод для определения слов с минимальной длиной
+        /// </summary>
+        /// <param name="str">предложение полученное из консоли</param>
+        /// <returns></returns>
+        static string[] MinWord(string str)
+        {
+            string[] array = str.Split(' ',',','.','-','_');
+            string help;
+            for (int i = 0; i < array.Length-1; i++)
+            {
+                for (int j = i+1; j < array.Length; j++)
+                {
+                    if (array[i].Length > array[j].Length)
+                    {
+                        help = array[i];
+                        array[i] = array[j];
+                        array[j] = help;
+                    }
+                }
+            }
+            string[] minstr = new string[array.Length];
+            minstr[0] = array[0];
+            for (int i = 1; i < minstr.Length; i++)
+            {              
+                if (minstr[0].Length!=array[i].Length)
+                {
+                    break;
+                }
+                else
+                {
+                    minstr[i] = array[i];
+                }
+            }
+            return minstr;
+        }
+        /// <summary>
+        /// Метод для определения слов с максимальной длиной
+        /// </summary>
+        /// <param name="str">предложение полученное из консоли</param>
+        /// <returns></returns>
+        static string[] MaxWord(string str)
+        {
+            string[] array = str.Split(' ',',' ,'.','-', '_');
+            string help;
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    if (array[i].Length < array[j].Length)
+                    {
+                        help = array[i];
+                        array[i] = array[j];
+                        array[j] = help;
+                    }
+                }
+            }
+            string[] minstr = new string[array.Length];
+            minstr[0] = array[0];
+            for (int i = 1; i < minstr.Length; i++)
+            {
+                if (minstr[0].Length != array[i].Length)
+                {
+                    break;
+                }
+                else
+                {
+                    minstr[i] = array[i];
+                }
+            }
+            return minstr;
+        }
+        /// <summary>
+        /// Метод для печати
+        /// </summary>
+        /// <param name="array"></param>
+        public static void Print(string[] array)
+        {
+            foreach (var item in array)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
