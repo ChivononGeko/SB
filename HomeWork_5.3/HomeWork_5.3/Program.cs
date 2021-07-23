1d112bbf18ad402001000905ceaea2b6f58d542e
ChivononGeko <74726135+ChivononGeko@users.noreply.github.com>
2021-07-23 16:10:00 +0600

    Add files via upload

--- HomeWork_5.3/HomeWork_5.3/Program.cs
+++ HomeWork_5.3/HomeWork_5.3/Program.cs
@@ -10,6 +10,38 @@ namespace HomeWork_5._3
     {
         static void Main(string[] args)
         {
+            Print(AntiCopi(Console.ReadLine()));
+        }
+        /// <summary>
+        /// Метод, убирающий повторения букв
+        /// </summary>
+        /// <param name="str">Слово</param>
+        /// <returns></returns>
+        public static char[] AntiCopi(string str)
+        {
+            str= str.ToLower();
+            char[] fword = str.ToCharArray();
+            char[] tword = new char[fword.Length];
+            tword[0] = fword[0];
+            for (int i = 0; i < fword.Length-1; i++)
+            {                
+                    if (fword[i]!=fword[i+1])
+                    {
+                        tword[i] = fword[i+1];
+                    }                
+            }
+            return tword;
+        }
+        /// <summary>
+        /// Метод для вывода в консоль
+        /// </summary>
+        /// <param name="word">Слово</param>
+        public static void Print(char[] word)
+        {
+            foreach (var item in word)
+            {
+                Console.Write(item);
+            }
         }
     }
 }
