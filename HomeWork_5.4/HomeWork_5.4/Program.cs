e27f75be29470fd2b06f3a8ba0b36427cb0f70b1
ChivononGeko <74726135+ChivononGeko@users.noreply.github.com>
2021-07-24 15:22:54 +0600

    Add files via upload

---
+++ HomeWork_5.4/HomeWork_5.4/Program.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork_5._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Укажите количество чисел в последовательности...");
+            int[] nums = Creat(Check(Console.ReadLine()));
+            Print(ArifmProgress(nums), GeometrProgress(nums));
+        }
+        /// <summary>
+        /// Метод для создания массива чисел
+        /// </summary>
+        /// <param name="n">Количество членов массива</param>
+        /// <returns></returns>
+        public static int[] Creat(int n)
+        {
+            Console.WriteLine("Введите числа...");
+            int[] nums = new int[n];
+            for (int i = 0; i < nums.Length; i++)
+            {
+                nums[i] = Check(Console.ReadLine());
+            }
+            return nums;
+        }
+        /// <summary>
+        /// Метод для проверки корректности данных
+        /// </summary>
+        /// <param name="n">Данные с консоли</param>
+        /// <returns></returns>
+        public static int Check(string n)
+        {
+            int result;
+            for (; ; )
+            {
+                if (int.TryParse(n, out result) && result > 0) break;
+                else
+                {
+                    Console.WriteLine("Не корректные данные. Повторите ввод.");
+                    n = Console.ReadLine();
+                }
+            }
+            return result;
+        }
+        /// <summary>
+        /// Метод для проверки на арифметическую прогрессию
+        /// </summary>
+        /// <param name="nums">Последовательность чисел</param>
+        /// <returns></returns>
+        public static bool ArifmProgress(int[] nums)
+        {
+            int d = nums[1] - nums[0];
+            bool isArifm = true;
+            for (int i = 0; i < nums.Length - 1; i++)
+            {
+                if (isArifm)
+                {
+                    if (nums[i + 1] - nums[i] != d)
+                    {
+                        isArifm = false;
+                    }
+                }
+            }
+            return isArifm;
+        }
+        /// <summary>
+        /// Метод проверки на геометрическую прогрессию
+        /// </summary>
+        /// <param name="nums">Последовательность чисел</param>
+        /// <returns></returns>
+        public static bool GeometrProgress(int[] nums)
+        {
+            int q = nums[1] / nums[0];
+            bool isGeometr = true;
+            for (int i = 0; i < nums.Length-1; i++)
+            {
+                if (isGeometr)
+                {
+                    if (nums[i + 1] / nums[i] != q)
+                    {
+                        isGeometr = false;
+                    }
+                }
+            }
+            return isGeometr;
+        }
+        /// <summary>
+        /// Метод вывода ответа
+        /// </summary>
+        /// <param name="isArifm"></param>
+        /// <param name="isGeometr"></param>
+        public static void Print(bool isArifm,bool isGeometr)
+        {
+            if (isArifm && isGeometr)
+            {
+                Console.WriteLine("Числовая последовательность одновременно арифметическая и геометричиская прогрессия !!!");
+            }
+            else if (isArifm && isGeometr==false)
+            {
+                Console.WriteLine("Последовательность является арифметической прогрессией !!!");
+            }
+            else if (isArifm == false && isGeometr)
+            {
+                Console.WriteLine("Последовательность является геометрической прогрессией !!!");
+            }
+            else
+            {
+                Console.WriteLine("Последовательность не прогрессия !!!");
+            }
+        }
+    }
+}
---
+++ HomeWork_5.5/HomeWork_5.5/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork_5._5
+{
+    class Program
+    {
+        public static int count=0;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите первое значение для функции..."); 
+            int m = Check(Console.ReadLine());
+            
+            Console.WriteLine("Введите второе значение для функции..."); 
+            int n = Check(Console.ReadLine());
+
+            Console.WriteLine($"Значение функции Аккермана при таких аргументах: {Akkerman(m,n)}");
+            Console.WriteLine($"Значение рекурсии: {count}");
+        }
+        public static int Check(string n)
+        {
+            int result;
+            for (; ; )
+            {
+                if (int.TryParse(n, out result) && result > 0) break;
+                else
+                {
+                    Console.WriteLine("Не корректные данные. Повторите ввод.");
+                    n = Console.ReadLine();
+                }
+            }
+            return result;
+        }
+        public static int Akkerman(int m,int n)
+        {
+            count++;
+            if (m == 0)
+            {
+                return n + 1;
+            }
+            if (m > 0 && n == 0)
+            {
+                return Akkerman(m - 1, 1);
+            }
+            return Akkerman(m - 1, Akkerman(m, n - 1));
+        }
+    }
+}
