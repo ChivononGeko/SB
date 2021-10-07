8ddf668a8031954593e8651bd2c43acd96f9f4e5
ChivononGeko <74726135+ChivononGeko@users.noreply.github.com>
2021-10-07 23:14:38 +0600

    Add files via upload

--- HomeWork_7/HomeWork_7/Program.cs
+++ HomeWork_7/HomeWork_7/Program.cs
@@ -16,36 +16,36 @@ namespace HomeWork_7
             do
             { 
             switch (Console.ReadKey().Key)
-            {              
-                case ConsoleKey.Enter:                  
+            {                          
+                case ConsoleKey.Enter:                  //Печать в консоль
                        rep.PrintToConsole();
                    break;
-                case ConsoleKey.Spacebar:
+                case ConsoleKey.Spacebar:               //Добавление
                        rep.Add(rep.FIlling());
                        rep.UnLoad();
                        rep.PrintToConsole();
                    break;
-                case ConsoleKey.Delete:
+                case ConsoleKey.Delete:                 //Удаление
                         Console.WriteLine("Введите имя по которому вы хотите удалить запись...");
                         string label = Console.ReadLine();
                         rep.Remove(label);                       
                         rep.UnLoad();
                         rep.PrintToConsole();
                         break;
-                case ConsoleKey.Backspace:
+                case ConsoleKey.Backspace:              //Редактирование
                         Console.WriteLine("Введите имя по которому вы хотите редактировать запись...");
                         label = Console.ReadLine();
                         rep.Edit(label);
                         rep.UnLoad();
                         rep.PrintToConsole();
-                        break;
-                    case ConsoleKey.Q:
+                        break; 
+                    case ConsoleKey.Q:                  //Упорядочивание
                         Console.WriteLine("Введите номер,по какому полю нужно упорядочить:\n1)По имени\n2)По фамилии\n3)По возрасту");
                         string n = Console.ReadLine();
                         rep.Sort(n);
                         rep.PrintToConsole();
                    break;
-                    case ConsoleKey.W:
+                    case ConsoleKey.W:                 //Вывод по диапозону дат
                         Console.WriteLine("Введите начальную и конечную даты...");
                         int a = int.Parse(Console.ReadLine());
                         int b = int.Parse(Console.ReadLine());
